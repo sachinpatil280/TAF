81eeb9897e82b423200c1efefb78d9fb35a3c58e
sachinpatil-medwing <sachin.patil@medwing.com>
2022-09-16 13:22:32 +0300

    Using webdriver-manager for handling updated browser versions drivers.

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -2,6 +2,12 @@ import pytest
 from selenium import webdriver
 from utilities.readProperties import read_config
 from fixtures.ui_fixtures import UI
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.ie.service import Service as IEService
 
 config_values = read_config('app_data')
 
@@ -9,13 +15,13 @@ config_values = read_config('app_data')
 @pytest.fixture()
 def ui(browser):
     if browser == "chrome" or browser == "Chrome":
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     elif browser == "firefox" or browser == "Firefox":
-        driver = webdriver.Firefox()
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     elif browser == "ie" or browser == "IE":
-        driver = webdriver.Ie()
+        driver = webdriver.Ie(service=IEService(IEDriverManager().install()))
     else:
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     driver.maximize_window()
     driver.get(config_values['baseurl'])
